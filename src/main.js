d3c891f94b8672341ed270eeefb332554655d4e3
ANoble25 <AWGR-Alex@comcast.net>
2018-05-21 13:45:50 -0700

    fixed an error

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './styles.css';
-import { starter } from './starter';
+import { starter } from './starter.js';
 
 $(document).ready(function() {
   $('#starter-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
-    var output = journal(goal);
+    var output = starter(goal);
     output.forEach(function(element) {
       $('#solution').append("<li>" + element + "</li>");
     });
