b785187505d115323e550675f8225b3f98b6dc2b
Guest User <Guest@epicodus-39.local>
2018-05-22 09:16:30 -0700

    add karma config

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,10 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
         loader: "eslint-loader"
       }
     ]
